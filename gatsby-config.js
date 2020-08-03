85276a843d14d98177cbc4db98b8a5d60bae2cf7
YUUSUKE OKAMOTO <38718737+YUUSUKE18@users.noreply.github.com>
2020-08-03 14:05:32 +0900

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: "Hello Yuusuke",
+    description: "This is Yuusuke's Portfolio site!",
+    siteUrl: "https://github.com/YUUSUKE18",
+    author: "Yuusuke Okamoto",
+  },
+  plugins: [`gatsby-plugin-postcss`],
+};
