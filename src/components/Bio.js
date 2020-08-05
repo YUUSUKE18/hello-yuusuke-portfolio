137f4dec04505d7ca3bdecf55043ec48014dc865
Yuusuke Okamoto <yuusuke.o.work.1018@gmail.com>
2020-08-05 12:36:10 +0900

    Change Files

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,7 +6,7 @@ const Bio = () => {
     <div id="about" className="py-3 px-2 sm:px-5">
       <div className="flex flex-col sm:flex-row">
         <div className="description sm:w-1/2 my-3 order-2 sm:order-1">
-          <h3 className="py-5 text-3xl font-bold">
+          <h3 className="py-5 text-3xl font-bold md:text-center">
             Hello! I'm Yuusuke. <br />
             <span className="text-xl font-bold">
               I'm a junior software engineer and creator.
--- src/components/Works.js
+++ src/components/Works.js
@@ -1,7 +1,29 @@
 import React from "react";
 
+import myWorks from "../data/myworks";
+
 const Works = () => {
-  return <div></div>;
+  return (
+    <>
+      <h2 className="text-2xl text-center">My Works</h2>
+      <div className="flex flex-col sm:flex-row sm:justify-center m-5">
+        {myWorks.map((mywork) => {
+          return (
+            <div class="max-w-sm rounded overflow-hidden shadow-lg mt-3 sm:mx-3">
+              <img class="w-full" src={mywork.image} alt={mywork.title} />
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">{mywork.title}</div>
+                <p class="text-gray-700 text-base">{mywork.description}</p>
+              </div>
+              <div class="px-6 py-4">
+                <a href={mywork.path}>Detail Page</a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Works;
