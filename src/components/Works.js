183bb729b998122157b98a5e2a77c70e1946a4d6
Okamoto Yuusuke <yuusuke_mbp@OkamotonoMBP.localdomain>
2020-10-09 18:21:35 +0900

    Change CSS

--- src/components/Works.js
+++ src/components/Works.js
@@ -2,8 +2,6 @@ import React from "react";
 
 import myWorks from "../data/myworks";
 
-import siteImg from "../images/Hello-React-Blog.png";
-
 const Works = () => {
   return (
     <>
@@ -11,14 +9,14 @@ const Works = () => {
       <div className="flex flex-col sm:flex-row sm:justify-center sm:flex-wrap m-5">
         {myWorks.map((mywork) => {
           return (
-            <div class="max-w-sm w-full rounded overflow-hidden shadow-lg p-4 m-auto mt-5 sm:mx-3 sm:w-1/3">
-              <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">{mywork.title}</div>
-                <p class="text-gray-700 text-base">{mywork.description}</p>
-                <p class="text-gray-700 text-base">{mywork.tech}</p>
+            <div className="max-w-sm w-full rounded overflow-hidden shadow-lg p-4 m-auto mt-5 sm:mx-3 sm:w-1/3">
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{mywork.title}</div>
+                <p className="text-gray-700 text-base">{mywork.description}</p>
+                <p className="text-gray-700 text-base">{mywork.tech}</p>
               </div>
-              <div class="px-6 py-4 text-center">
-                <div class="link">
+              <div className="flex flex-col px-6 py-4">
+                <div className="text-center">
                   <a
                     href={mywork.path}
                     className="bg-green-300 text-white p-3 rounded hover:bg-transparent hover:text-green-300 hover:border-solid hover:border-green-300"
@@ -26,10 +24,10 @@ const Works = () => {
                     Detail Page
                   </a>
                 </div>
-                <div class="link">
+                <div className="text-center">
                   <a
                     href={mywork.github}
-                    className="bg-green-300 text-white p-3 mt-3 rounded hover:bg-transparent hover:text-green-300 hover:border-solid hover:border-green-300"
+                    className="bg-green-300 text-white p-3 rounded hover:bg-transparent hover:text-green-300 hover:border-solid hover:border-green-300"
                   >
                     Source
                   </a>
